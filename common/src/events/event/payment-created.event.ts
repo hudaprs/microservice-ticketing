2eaf2e58a7ecd2e29a54d6453eb10f52d6553b78
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2022-05-27 16:09:59 +0700

    Updates

---
+++ common/src/events/event/payment-created.event.ts
@@ -0,0 +1,13 @@
+// Subjects
+import { Subjects } from '../subjects'
+
+interface PaymentCreatedEvent {
+	subject: Subjects.PaymentCreated
+	data: {
+		id: string
+		orderId: string
+		stripeId: string
+	}
+}
+
+export { PaymentCreatedEvent }
